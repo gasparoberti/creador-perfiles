a5a04c43e0385098dc5d53eb1403785be6c04b51
Gaspar <obertigaspar@gmail.com>
2021-12-23 00:55:40 -0300

    se crearon las funcionalidades para que realice el sorteo y limpie la tabla. en una segunda version se añadirán las funcionalidades para descargar como pdf y copiar en el portapapeles

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 
+const familia = ['MADRE-PADRE SOLTERO','PAREJA','PERSONA SOLA', 'FAMILIA 2H.', 'FAMILIA 3 H.', 'AMIGUES', 'FAMILIA ENS. 2 H.', 'MADRE-PADRE SOLTERO +'];
+const edad_adultes = ['30-35','35-40','40-45', '45-50', '50-55', '55-60', '60-65'];
+const edad_gurises = ['0-5','5-10','10-15', '15-20', '20-25'];
+const hobbies = ['COCINA','ARTE','OFICIO', 'DEPORTIVO', 'LECTURA-ESCRITURA', 'MÚSICA', 'NINGUNO'];
+const trabajo = ['Home Adm.','Home Emp.','Fuera de Casa', 'Cuidador'];
+const localidad = ['FRANCK','RINCÓN','RECONQUISTA', 'SANTO TOMÉ', 'PARANÁ', 'ARROYO', 'LEYES', 'SANTA FE', 'SAUCE VIEJO', 'MONTE VERA', 'RECREO'];
+const caracteristica = ['MADERA','HORMIGÓN','LADRILLO', 'PILETA-QUINCHO', 'VERDE', 'FUTURA PA', 'COLORES', 'ILUMINACIÓN', 'TECHOS ALTOS', 'NINGUNA'];
+const procrear_2_dorm = ['ALFONSINA','CRIOLLA','MILAGRO', 'JUANA', 'BICENTENARIA', 'AIME'];
+const procrear_1_dorm = ['COMPAÑERA I','COMPAÑERA II','FEDERAL I', 'FEDERAL II'];
+
 window.onload = function(e){ 
-    const familia = ['MADRE-PADRE SOLTERO','PAREJA','PERSONA SOLA', 'FAMILIA 2H.', 'FAMILIA 3 H.', 'AMIGUES', 'FAMILIA ENS. 2 H.', 'MADRE-PADRE SOLTERO +'];
-    const edad_adultes = ['30-35','35-40','40-45', '45-50', '50-55', '55-60', '60-65'];
-    const edad_gurises = ['0-5','5-10','10-15', '15-20', '20-25'];
-    const hobbies = ['COCINA','ARTE','OFICIO', 'DEPORTIVO', 'LECTURA-ESCRITURA', 'MÚSICA', 'NINGUNO'];
-    const trabajo = ['Home Adm.','Home Emp.','Fuera de Casa', 'Cuidador'];
-    const localidad = ['FRANCK','RINCÓN','RECONQUISTA', 'SANTO TOMÉ', 'PARANÁ', 'ARROYO', 'LEYES', 'SANTA FE', 'SAUCE VIEJO', 'MONTE VERA', 'RECREO'];
-    const caracteristica = ['MADERA','HORMIGÓN','LADRILLO', 'PILETA-QUINCHO', 'VERDE', 'FUTURA PA', 'COLORES', 'ILUMINACIÓN', 'TECHOS ALTOS', 'NINGUNA'];
-    const procrear_2_dorm = ['ALFONSINA','CRIOLLA','MILAGRO', 'JUANA', 'BICENTENARIA', 'AIME'];
-    const procrear_1_dorm = ['COMPAÑERA I','COMPAÑERA II','FEDERAL I', 'FEDERAL II'];
 
     eventos_tr()
-    console.log(sorteo_perfil(procrear_1_dorm))
+}
+
+
+function limpiar_tabla() { debugger
+    document.getElementById("td_familia").textContent  = ""
+    document.getElementById("td_edad_adultes").textContent  = ""
+    document.getElementById("td_edad_gurises").textContent  = ""
+    document.getElementById("td_hobbies").textContent  = ""
+    document.getElementById("td_trabajo").textContent  = ""
+    document.getElementById("td_localidad").textContent  = ""
+    document.getElementById("td_caracteristica").textContent  = ""
+    document.getElementById("td_procrear").textContent  = ""
+}
+
+function completar_td(nombre_categoría, array_categoría) {
+    document.getElementById("td_" + nombre_categoría).innerHTML = sorteo_componente(array_categoría)
+}
+
+function obtener_categoria_procrear() {
+    var resultado
+    if (document.getElementById("slct_procrear").value == "1 Dorm.")
+        resultado = procrear_1_dorm
+    else 
+        resultado = procrear_2_dorm
+
+    return resultado
 }
 
 function eventos_tr() {
@@ -25,7 +51,18 @@ function eventos_tr() {
     mostrar_fila("tr_procrear", "chk_procrear")
 }
 
-function sorteo_perfil(categoria) {
+function sorteo_perfil() {
+    completar_td("familia", familia)
+    completar_td("edad_adultes", edad_adultes)
+    completar_td("edad_gurises", edad_gurises)
+    completar_td("hobbies", hobbies)
+    completar_td("trabajo", trabajo)
+    completar_td("localidad", localidad)
+    completar_td("caracteristica", caracteristica)
+    completar_td("procrear", obtener_categoria_procrear())
+}
+
+function sorteo_componente(categoria) {
     var rand = Math.random()*categoria.length | 0;
     var rValue = categoria[rand];
     return rValue;
